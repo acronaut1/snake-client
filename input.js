a3e2f11c0ded394a7aba559526009ab108860f64
acronaut1 <opruski@ryerson.ca>
2021-11-03 15:01:02 -0400

    ADD input and move code to its ownplace

---
+++ input.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // code here..
+  
+  
+}
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,7 +1,11 @@
 
+//const { stdin } = require('process');
+const { setupInput } = require('./input');
 const {connect} = require('./client');
+
+// setup interface..
 
 console.log("Connecting ...");
 connect();
-
+setupInput();
 
